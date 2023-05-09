59338ab4016531ee82650faa1f0fea009c8eaef1
DJ Hemath <djhemath@gmail.com>
2023-05-09 11:52:42 +0530

    fix: server and client font class mismatch

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,12 +1,14 @@
 import { PropsWithChildren } from "react";
 import Header from "./components/Header/Header";
 import styles from "./layout.module.css";
+import { Quicksand } from 'next/font/google'
+const quicksand = Quicksand({ subsets: ['latin'] })
 
 type LayoutProps = {} & PropsWithChildren;
 
 export default function Layout({ children }: LayoutProps) {
     return (
-        <div className={styles.layout}>
+        <div className={styles.layout + ' ' + quicksand.className}>
             <Header />
             <main className="container">
                 {children}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,10 @@
 import Layout from "@/layout/layout";
 import { AppProps } from "next/app";
 import './styles.css'
-import { Quicksand } from 'next/font/google'
-
-const quicksand = Quicksand({ subsets: ['latin'] })
-
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <div className={quicksand.className}>
+        <div>
             <Layout>
                 <Component {...pageProps} />
             </Layout>
